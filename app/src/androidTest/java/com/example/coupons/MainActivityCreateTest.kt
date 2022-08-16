b2d9957a9630cb697ada329e4bdf515d96b3544d
Luisa Velasquez <luvlasquez96@gmail.com>
2022-08-16 11:21:20 -0500

    Agregar combios Test

---
+++ app/src/androidTest/java/com/example/coupons/MainActivityCreateTest.kt
@@ -0,0 +1,4 @@
+package com.example.coupons
+
+class MainActivityCreateTest {
+}
--- app/src/test/java/com/example/coupons/common/utils/CouponsUtilsKtTest.kt
+++ app/src/test/java/com/example/coupons/common/utils/CouponsUtilsKtTest.kt
@@ -2,11 +2,72 @@ package com.example.coupons.common.utils
 
 import org.junit.Assert.*
 import org.junit.Test
+import com.example.coupons.R
+import com.example.coupons.common.entities.CouponEntity
 
 class CouponsUtilsKtTest{
     @Test
 
-    fun validateTextCodesuccessTest(){
-        assertTrue(true)
+    fun validateTextCodeSuccessTest(){
+        val code="Welcome"
+        assertTrue(validateTextCode(code))
+    }
+    @Test
+
+    fun validateTextCodeEmptyFailedTest(){
+        val code="hola soy luisa velasquez"
+        assertFalse(validateTextCode(code))
+    }
+
+    @Test
+
+    fun getMsgErrorByCodeNullTest(){
+        val errorCode=null
+        val expectedValue= R.string.error_unknow
+
+        val result= getMsgErrorByCode(errorCode)
+
+        assertEquals("Error al evaluar un null", expectedValue,result)
+    }
+    @Test
+
+    fun getMsgErrorByCodeExistTest(){
+        val errorCode=Constants.ERROR_EXIST
+        val expectedValue= R.string.Error_unique_code
+
+        val result= getMsgErrorByCode(errorCode)
+
+        assertEquals("Error al evaluar un cupon existente",expectedValue,result)
+    }
+    @Test
+
+    fun getMsgErrorByCodeLengthTest(){
+        val errorCode=Constants.ERROR_LENGTH
+        val expectedValue= R.string.Error_invalid_length
+
+        val result= getMsgErrorByCode(errorCode)
+
+        assertEquals("Error al evaluar la longitud valida",expectedValue,result)
+        assertNotEquals("El resurso no existe",-1,result)
+    }
+    @Test
+
+    fun checkNotNullCouponTest(){
+        val coupon= CouponEntity(code="ANDROID", description = "Cursos a $9.99 USD")
+        assertNotNull(coupon)
+    }
+    @Test
+
+    fun checkGroupSuccessTest(){
+        val aNames= arrayOf("Luisa", "Nicole", "Maria")//valor actual
+        val bNames= arrayOf("Luisa","Nicole","Maria")//valor esperado
+        assertArrayEquals(bNames,aNames)
+    }
+    @Test
+
+    fun checkGroupFailTest(){
+        val aNames= arrayOf("Luisa", "Nicolas", "Maria")//valor actual
+        val bNames= arrayOf("Luisa","Nicole","Maria")//valor esperado
+        assertNotEquals(bNames,aNames)
     }
 }
